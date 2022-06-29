0dc53d4bd73097f59dbadb8ea7798bc34196654b
[prasanna] <prasanna>
2022-06-29 20:10:47 +0530

    [prasanna]: Add multi threads to addressbook

--- ADO.NETAddressBook/BookAnalyser.cs
+++ ADO.NETAddressBook/BookAnalyser.cs
@@ -12,105 +12,107 @@ namespace ADO.NETAddressBook
     public class BookAnalyser
     {
         public static string DBPath = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AddressBook;";
-        public void CreateNewContactDetails()
+
+
+        /*public void CreateNewContactDetails()
+{
+SqlConnection connect = new SqlConnection(DBPath);
+using (connect)
+{
+connect.Open();
+ADO.NETAddressBook.Book model = new ADO.NETAddressBook.Book();
+Console.WriteLine("Enter First Name");
+model.FIRSTNAME = Console.ReadLine();
+Console.WriteLine("Enter Last Name");
+model.LASTNAME = Console.ReadLine();
+Console.WriteLine("Enter Address ");
+model.ADDRESSs = Console.ReadLine();
+Console.WriteLine("Enter City ");
+model.CITY = Console.ReadLine();
+Console.WriteLine("Enter State ");
+model.STATE = Console.ReadLine();
+Console.WriteLine("Enter Zip Code ");
+model.ZIP = Console.ReadLine();
+Console.WriteLine("Enter Phone ");
+model.PHONENO = Console.ReadLine();
+Console.WriteLine("Enter Email ");
+model.EMAIL = Console.ReadLine();
+SqlCommand sql = new SqlCommand("SP_AddressBook", connect);
+sql.CommandType = CommandType.StoredProcedure;
+sql.Parameters.AddWithValue("@FIRSTNAME", model.FIRSTNAME);
+sql.Parameters.AddWithValue("@LASTNAME", model.LASTNAME);
+sql.Parameters.AddWithValue("@ADDRESSs", model.ADDRESSs);
+sql.Parameters.AddWithValue("@CITY", model.CITY);
+sql.Parameters.AddWithValue("@STATE", model.STATE);
+sql.Parameters.AddWithValue("@ZIP", model.ZIP);
+sql.Parameters.AddWithValue("@PHONENO", model.PHONENO);
+sql.Parameters.AddWithValue("@EMAIL", model.EMAIL);
+sql.ExecuteNonQuery();
+Console.WriteLine("Record created successfully.");
+connect.Close();
+}
+}*/
+        public void getDataFromAddressBook()
         {
             SqlConnection connect = new SqlConnection(DBPath);
+            ADO.NETAddressBook.Book value = new ADO.NETAddressBook.Book();
             using (connect)
             {
                 connect.Open();
-                ADO.NETAddressBook.Book model = new ADO.NETAddressBook.Book();
-                Console.WriteLine("Enter First Name");
-                model.FIRSTNAME = Console.ReadLine();
-                Console.WriteLine("Enter Last Name");
-                model.LASTNAME = Console.ReadLine();
-                Console.WriteLine("Enter Address ");
-                model.ADDRESSs = Console.ReadLine();
-                Console.WriteLine("Enter City ");
-                model.CITY = Console.ReadLine();
-                Console.WriteLine("Enter State ");
-                model.STATE = Console.ReadLine();
-                Console.WriteLine("Enter Zip Code ");
-                model.ZIP = Console.ReadLine();
-                Console.WriteLine("Enter Phone ");
-                model.PHONENO = Console.ReadLine();
-                Console.WriteLine("Enter Email ");
-                model.EMAIL = Console.ReadLine();
-                SqlCommand sql = new SqlCommand("SP_AddressBook", connect);
-                sql.CommandType = CommandType.StoredProcedure;
-                sql.Parameters.AddWithValue("@FIRSTNAME", model.FIRSTNAME);
-                sql.Parameters.AddWithValue("@LASTNAME", model.LASTNAME);
-                sql.Parameters.AddWithValue("@ADDRESSs", model.ADDRESSs);
-                sql.Parameters.AddWithValue("@CITY", model.CITY);
-                sql.Parameters.AddWithValue("@STATE", model.STATE);
-                sql.Parameters.AddWithValue("@ZIP", model.ZIP);
-                sql.Parameters.AddWithValue("@PHONENO", model.PHONENO);
-                sql.Parameters.AddWithValue("@EMAIL", model.EMAIL);
-                sql.ExecuteNonQuery();
-                Console.WriteLine("Record created successfully.");
+                string query = "Select * from AddressBook";
+                SqlCommand command = new SqlCommand(query, connect);
+                SqlDataReader data = command.ExecuteReader();
+                if (data.HasRows)
+                {
+                    while (data.Read())
+                    {
+                        value.ID = data.GetInt32(0);
+                        value.FIRSTNAME = data.GetString(1);
+                        value.LASTNAME = data.GetString(2);
+                        value.ADDRESSs = data.GetString(3);
+                        value.CITY = data.GetString(4);
+                        value.STATE = data.GetString(5);
+                        value.ZIP = data.GetString(6);
+                        value.PHONENO = data.GetString(7);
+                        value.EMAIL = data.GetString(8);
+
+                        Console.WriteLine(value.ID + "\n " + value.FIRSTNAME + " \n" + value.LASTNAME + "\n " + value.ADDRESSs + "\n " + value.CITY + "\n " + value.STATE + "\n " + value.ZIP + " \n" + value.PHONENO + "\n " + value.EMAIL);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Records not found in Database.");
+                }
+                data.Close();
                 connect.Close();
             }
         }
-        /*public void getDataFromAddressBook()
-        {
-                SqlConnection connect = new SqlConnection(DBPath);
-                ADO.NETAddressBook.Book value = new ADO.NETAddressBook.Book();
-                using (connect)
-                {
-                    connect.Open();
-                    string query = "Select * from AddressBook";
-                    SqlCommand command = new SqlCommand(query, connect);
-                    SqlDataReader data = command.ExecuteReader();
-                    if (data.HasRows)
-                    {
-                        while (data.Read())
-                        {
-                            value.ID = data.GetInt32(0);
-                            value.FIRSTNAME = data.GetString(1);
-                            value.LASTNAME = data.GetString(2);
-                            value.ADDRESSs = data.GetString(3);
-                            value.CITY = data.GetString(4);
-                            value.STATE = data.GetString(5);
-                            value.ZIP = data.GetString(6);
-                            value.PHONENO = data.GetString(7);
-                            value.EMAIL = data.GetString(8);
 
-                            Console.WriteLine(value.ID + "\n " + value.FIRSTNAME + " \n" + value.LASTNAME + "\n " + value.ADDRESSs + "\n " + value.CITY + "\n " + value.STATE + "\n " + value.ZIP + " \n" + value.PHONENO + "\n " + value.EMAIL);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Records not found in Database.");
-                    }
-                    data.Close();
-                    connect.Close();
-                }
-        }*/
-        
         public void UpdateDetails()
         {
-                SqlConnection connect = new SqlConnection(DBPath);
-                try
+            SqlConnection connect = new SqlConnection(DBPath);
+            try
+            {
+                using (connect)
                 {
-                    using (connect)
-                    {
-                        Console.WriteLine("Enter name of Person:");
-                        string name = Console.ReadLine();
-                        Console.WriteLine("Enter contact to update:");
-                        string phone = Console.ReadLine();
-                        connect.Open();
-                        string query = "update AddressBook set PHONENO =" + phone + "where FIRSTNAME='" + name + "'";
-                        SqlCommand command = new SqlCommand(query, connect);
-                        command.ExecuteNonQuery();
-                        Console.WriteLine("Details updated successfully.");
+                    Console.WriteLine("Enter name of Person:");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter contact to update:");
+                    string phone = Console.ReadLine();
+                    connect.Open();
+                    string query = "update AddressBook set PHONENO =" + phone + "where FIRSTNAME='" + name + "'";
+                    SqlCommand command = new SqlCommand(query, connect);
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("Details updated successfully.");
 
-                    }
                 }
-                catch (FormatException)
-                {
-                    Console.WriteLine("-------\nError:Records are not updated.\n-----");
-                }
-                connect.Close();
-            
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("-------\nError:Records are not updated.\n-----");
+            }
+            connect.Close();
+
         }
 
         public void DeleteDetails()
@@ -129,7 +131,43 @@ namespace ADO.NETAddressBook
             }
 
         }
-        
-        
+        public void CreateNewContact(Book model)
+        {
+            SqlConnection connect = new SqlConnection(DBPath);
+            lock (this)
+                using (connect)
+                {
+                    connect.Open();
+                    SqlCommand sql = new SqlCommand("SP_AddressBook", connect);
+                    sql.CommandType = CommandType.StoredProcedure;
+                    sql.Parameters.AddWithValue("@FIRSTNAME", model.FIRSTNAME);
+                    sql.Parameters.AddWithValue("@LASTNAME", model.LASTNAME);
+                    sql.Parameters.AddWithValue("@ADDRESSs", model.ADDRESSs);
+                    sql.Parameters.AddWithValue("@CITY", model.CITY);
+                    sql.Parameters.AddWithValue("@STATE", model.STATE);
+                    sql.Parameters.AddWithValue("@ZIP", model.ZIP);
+                    sql.Parameters.AddWithValue("@PHONENO", model.PHONENO);
+                    sql.Parameters.AddWithValue("@EMAIL", model.EMAIL);
+                    sql.ExecuteNonQuery();
+                    Console.WriteLine("Record created successfully.");
+                    connect.Close();
+                }
+
+
+        }
+        public void AddMultipleEmployee(List<Book> data)
+        {
+            data.ForEach(details =>
+            {
+                Thread thread = new Thread(() =>
+                {
+                    Console.WriteLine("Thread Start Time: " + DateTime.Now);
+                    this.CreateNewContact(details);
+                    Console.WriteLine("Contact Added: " + details.FIRSTNAME);
+                    Console.WriteLine("Thread End Time: " + DateTime.Now);
+                });
+                thread.Start();
+            });
+        }
     }
 }
--- ADO.NETAddressBook/Program.cs
+++ ADO.NETAddressBook/Program.cs
@@ -5,11 +5,39 @@ namespace ADO.NETAddressBook
     {
         public static void Main(string[] args)
         {
-            BookAnalyser exp = new BookAnalyser();
-           // exp.getDataFromAddressBook();
-            //exp.UpdateDetails();
-            //exp.DeleteDetails();
-            exp.CreateNewContactDetails();
+            BookAnalyser count = new BookAnalyser();
+            // count.getDataFromAddressBook();
+            //count.UpdateDetails();
+            //count.DeleteDetails();
+            //count.CreateNewContactDetails();
+            List<Book> list = new List<Book>();
+            Console.WriteLine("Enter number of contacts to Add");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i < number)
+            {
+                ADO.NETAddressBook.Book exp = new ADO.NETAddressBook.Book();
+
+                Console.WriteLine("Enter First Name");
+                exp.FIRSTNAME = Console.ReadLine();
+                Console.WriteLine("Enter Last Name");
+                exp.LASTNAME = Console.ReadLine();
+                Console.WriteLine("Enter Address ");
+                exp.ADDRESSs = Console.ReadLine();
+                Console.WriteLine("Enter City ");
+                exp.CITY = Console.ReadLine();
+                Console.WriteLine("Enter State ");
+                exp.STATE = Console.ReadLine();
+                Console.WriteLine("Enter Zip Code ");
+                exp.ZIP = Console.ReadLine();
+                Console.WriteLine("Enter Phone ");
+                exp.PHONENO = Console.ReadLine();
+                Console.WriteLine("Enter Email ");
+                exp.EMAIL = Console.ReadLine();
+                list.Add(exp);
+                i++;
+            }
+           count.AddMultipleEmployee(list);
         }
     }
 }
